ce65ecc2272a91abdba4b47fc0748c89eef0af08
zaenury <adhiimzaenury@gmail.com>
2022-07-26 16:43:32 +0800

    Typechecking with propTypes

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,5 +1,24 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
+
 export default function Button(props) {
   return <div>Button</div>;
 }
+
+// Typechecking With PropTypes
+Button.propTypes = {
+  // You can ensure that your prop is limited to specific values by treating
+  // it as an enum.
+  type: propTypes.oneOf(["button"], ["link"]),
+  onClick: propTypes.func,
+  target: propTypes.string,
+  className: propTypes.string,
+  isDisabled: propTypes.bool,
+  isLoading: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  hasShadow: propTypes.bool,
+};
